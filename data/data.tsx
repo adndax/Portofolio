3fc429efc471b35e15506c34c0599eec1b92435f
adndax <adindaputri0517@gmail.com>
2025-01-28 13:02:34 +0700

    feat: create data for navbar and home page

---
+++ data/data.tsx
@@ -0,0 +1,10 @@
+export const navItems = [
+    { name: "Skills", link: "#skills" },
+    { name: "Projects", link: "#projects" },
+    { name: "Experiences", link: "#experiences" },
+    { name: "Contact", link: "#contact" },
+];
+
+export const words = [
+    'Portfolio', 'Work', 'Website', 'Creation'
+]
