da04fd3cda9d043fdb6e8e215d08ab329c6e7981
adndax <adindaputri0517@gmail.com>
2025-02-01 18:14:20 +0700

    feat: create project page

---
+++ src/app/component/Projects.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import { ThreeDCardDemo } from "./ui/3DCard";
+import Image from "next/image";
+import { projects } from "@data";
+
+const Projects = () => {
+  return (
+    <section className='relative w-screen overflow-x-hidden'>
+      <div id='projects' className='flex flex-col ml-7 mr-7 mt-14 sm:mt-48 pt-24 sm:pt-28'>
+        <h1 className='uppercase font-bold text-4xl text-center mb-10'> 
+          Projects
+        </h1>
+        <div className='flex flex-wrap gap-8 justify-center'>
+          {projects.map((project, idx) => (
+            <ThreeDCardDemo key={idx} {...project} />
+          ))}
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default Projects
