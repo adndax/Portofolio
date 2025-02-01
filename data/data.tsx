63138e3084e27b11a15cae1f5229e57648596f15
adndax <adindaputri0517@gmail.com>
2025-02-01 14:50:03 +0700

    feat: create data for projects

--- data/data.tsx
+++ data/data.tsx
@@ -1,11 +1,9 @@
 import { FaHtml5 } from "react-icons/fa";
-import { IoLogoCss3 } from "react-icons/io5";
-import { IoLogoJavascript } from "react-icons/io5";
+import { IoLogoCss3, IoLogoJavascript } from "react-icons/io5";
 import { SiTypescript } from "react-icons/si";
-import { FaPython } from "react-icons/fa";
+import { FaPython, FaReact } from "react-icons/fa";
 import { PiFileC } from "react-icons/pi";
-import { FaReact } from "react-icons/fa";
-import { RiNextjsFill } from "react-icons/ri";
+import { RiNextjsFill, RiTailwindCssFill  } from "react-icons/ri";
 import { SiVite } from "react-icons/si";
 import { FaGitAlt } from "react-icons/fa6";
 import { FaGithub } from "react-icons/fa";
@@ -13,7 +11,7 @@ import { FaGithub } from "react-icons/fa";
 export const navItems = [
     { name: "Skills", link: "#skills" },
     { name: "Projects", link: "#projects" },
-    { name: "Experiences", link: "#experiences" },
+    { name: "Activities", link: "#activities" },
     { name: "Contact", link: "#contact" },
 ];
 
@@ -53,6 +51,10 @@ export const skills2 = [
         icon: <SiVite size={50}/>,
         name: "Vite",
     },
+    {
+        icon: <RiTailwindCssFill size={50}/>,
+        name: "Tailwind",
+    },
 ];
 
 export const skills3 = [
@@ -73,3 +75,30 @@ export const skills3 = [
         name: "Github",
     },
 ]
+
+export const projects = [
+    {
+        title: "Grootopia: Plant Management Desktop Apps",
+        description: "Designed the graphical user interface (GUI) for Grootopia, a desktop application aimed at helping users manage their plants and track growth.",
+        jobs: "GUI Developer",
+        imageSrc: "/grootopia.png",
+        link: "https://github.com/adndax/IF2150-2024-K01-G04-Grootopia",
+        icons: [<FaGithub size={20} />, <FaPython size={20} />]
+      },
+      {
+        title: "OSKM ITB 2024: Official Website for ITB's New Student",
+        jobs: "Front End Developer",
+        description: "Developed the login form for the OSKM ITB 2024 website, enabling users to securely log in to access event-related information and resources.",
+        imageSrc: "/oskm.jpg",
+        link: "https://github.com/adndax",
+        icons: [<FaGithub size={20} />, <FaReact size={20} />, <RiNextjsFill size={20} />, <SiTypescript size={20} />, <RiTailwindCssFill size={20} />]
+      },
+      {
+        title: "Simsalabim: Image and Music Retrieval Website",
+        description: "A platform designed to provide efficient image retrieval and music data retrieval, enabling users to search and discover media based on different attributes.",
+        jobs: "Full Stack Developer",
+        imageSrc: "/simsalabim.png",
+        link: "https://github.com/nomisnoumi/tubesalgeo02",
+        icons: [<FaGithub size={20} />, <FaReact size={20} />, <SiTypescript size={20} />, <IoLogoCss3 size={20} />, <FaPython size={20} />,]
+      },
+  ];
