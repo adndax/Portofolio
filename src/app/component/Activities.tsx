0b8befb087803c03a970b914f640263d1780cd51
adndax <adindaputri0517@gmail.com>
2025-02-01 20:32:59 +0700

    feat: add gradient background

--- src/app/component/Activities.tsx
+++ src/app/component/Activities.tsx
@@ -3,9 +3,9 @@ import { AnimatedTestimonialsDemo } from './ui/AnimatedActivities'
 
 const Activities = () => {
   return (
-    <section className='relative w-screen overflow-x-hidden'>
-      <div id='activities' className='flex flex-col ml-7 mr-7 mt-14 sm:mt-48 pt-24 sm:pt-32 justify-center items-center'>
-        <h1 className='uppercase font-bold text-4xl text-center mb-10'> 
+    <section className='relative w-screen overflow-x-hidden bg-gradient-to-bl from-[#3e1753] via-black to-black'>
+      <div id='activities' className='flex flex-col mt-14 sm:mt-48 pt-24 sm:pt-32 justify-center items-center'>
+        <h1 className='uppercase font-bold text-4xl text-center sm:mb-10'> 
           Activities
         </h1>
       </div>
--- src/app/component/Projects.tsx
+++ src/app/component/Projects.tsx
@@ -5,7 +5,7 @@ import { projects } from "@data";
 
 const Projects = () => {
   return (
-    <section className='relative w-screen overflow-x-hidden'>
+    <section className='relative w-screen overflow-x-hidden bg-gradient-to-br from-black via-black to-[#3f1753]'>
       <div id='projects' className='flex flex-col ml-7 mr-7 mt-14 sm:mt-48 pt-24 sm:pt-28'>
         <h1 className='uppercase font-bold text-4xl text-center mb-10'> 
           Projects
